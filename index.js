d941f699790ef53c38ff494fc6e088ed528255f3
Robert-OP <popescu.robert.octavian@gmail.com>
2018-10-03 14:58:03 +0200

    changed something

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 // for production look for the port heroku lets us use, otherwise use port 5000 for development
