924d302ca8d6842177adbf3d94ed70c052578410
Robert-OP <popescu.robert.octavian@gmail.com>
2018-10-09 19:42:15 +0200

    stripe update-credits middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -10,6 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// middlewares that operate on the incoming requests before they are sent off to the request handlers
+app.use(bodyParser.json()); // this will parse the body and then assign it to the req.body property of the incoming request object
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,8 +22,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// connected to the arrow function in authRoutes
+// connected to the arrow function in authRoutes & payRoutes
 require("./routes/authRoutes")(app);
+require("./routes/payRoutes")(app);
 
 // for production look for the port heroku lets us use, otherwise use port 5000 for development
 const PORT = process.env.PORT || 5000;
