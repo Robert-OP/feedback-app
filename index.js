481076b23ee217d0bdd166188f29655d02ae7704
Robert-OP <popescu.robert.octavian@gmail.com>
2018-10-10 12:15:36 +0200

    client routes & heroku production setup

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/payRoutes")(app);
 
+// ran only in production (heroku)
+if (process.env.NODE_ENV === "production") {
+  // 1st express will serve production assets (main.js or main.css files)
+  app.use(express.static("client/build"));
+
+  /* 2nd if express cannot recognize the route will serve index.html
+    such that the react router side app is responsible for the route
+    kick the user to the client-side of the app 
+  */
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // for production look for the port heroku lets us use, otherwise use port 5000 for development
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
