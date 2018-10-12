4bb64a604e932cb5cd485f86b06f0576af6bb60e
Robert-OP <popescu.robert.octavian@gmail.com>
2018-10-12 22:58:41 +0200

    client-side survey form with redux-form #1

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SurveyFormReview extends Component {
+  render() {
+    return <div>Survey Form Review</div>;
+  }
+}
+
+export default SurveyFormReview;
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,16 @@
+// SurveyNew shows SurveyForm & SurveyFormReview
+import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
